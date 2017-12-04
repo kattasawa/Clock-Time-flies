f60a9824452998a94ac15fb3c704e2e6bf9b1833
Katarina Sjölund <katarina.sjolund@outlook.com>
2017-12-04 22:50:11 +0100

    latest update

--- code/src/index.js
+++ code/src/index.js
@@ -3,16 +3,63 @@ import ReactDOM from "react-dom"
 import "./index.css"
 
 class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval (
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date ()
+    });
+  }
+
+  handleClickPause = () => {
+    clearInterval(this.timerID)
+  }
+
+  handleClickRun = () => {
+    this.timerID = setInterval (
+      () => this.tick(),
+      1000
+    );
+  }
+
+
 
   render() {
     return (
       <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+        {/* <h1>Hello, world!</h1> */}
+        <div className="clock-box" >
+          <h1> {this.state.date.toLocaleTimeString()} </h1>
+        </div>
+        <div className="buttons-container">
+          <button onClick={this.handleClickPause}>
+            PAUSE
+          </button>
+          <button onClick={this.handleClickRun}>
+            RUN
+          </button>
+        </div>
       </div>
     )
   }
 
 }
 
-ReactDOM.render(<Clock date={new Date()} />, document.getElementById("root"))
+ReactDOM.render(
+  <Clock />,
+  document.getElementById("root")
+);
